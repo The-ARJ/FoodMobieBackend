55c74812a9311e68a1a1a7a506cd7834fb6b9199
The-ARJ <joshiaayush871@gmail.com>
2022-12-30 08:00:14 +0545

    user model updated

--- models/users.js
+++ models/users.js
@@ -1,19 +1,35 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-  username: {
+  email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
-  isAdmin: {
-    type: Boolean,
-    default: false
-  }
+  name: {
+    type: String,
+    required: true,
+  },
 });
+
+userSchema.methods.hashPassword = function () {
+  this.password = bcrypt.hashSync(this.password, 8);
+};
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.methods.generateJWT = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
