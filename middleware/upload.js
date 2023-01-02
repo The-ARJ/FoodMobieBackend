84fb81ad81761457777bf676445ca6dfd62e17ce
The-ARJ <joshiaayush871@gmail.com>
2023-01-02 09:46:39 +0545

    multer implemented

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,6 @@
 const multer = require('multer')
 const path = require('path')
+const { off } = require('process')
 
 
 const storage = multer.diskStorage({
@@ -12,7 +13,16 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({ storage: storage })
+const imageFileFilter = (res,file,cb)=>{
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+        return cb(new Error('File format not supported'),false)
+    }
+    cb(null,true)
+}
+
+const upload = multer({ storage: storage,
+fileFilter:imageFileFilter 
+})
 
 
 module.exports = upload
