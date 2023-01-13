a932416e669d38d6508675a1ee66b5e6a7f27c9e
The-ARJ <joshiaayush871@gmail.com>
2023-01-13 19:57:48 +0545

    user model improved

--- models/User.js
+++ models/User.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        minlength:[4,'Username should be longer than 4 characters']
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: [true, 'Username matches with existing users'],
+        minLength: 6
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: [true, 'Password is required']
     },
-    role:{
-        type:String,
-        enum:['Admin','User'],
-        default:'User'
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'manager'],
+        default: 'user'
     },
     profile: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Profile'
-  }
-},{timestamps:true});
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Profile'
+    }
+}, { timestamps: true })
 
-module.exports = mongoose.model('User',userSchema)
+module.exports = mongoose.model('User', userSchema)
