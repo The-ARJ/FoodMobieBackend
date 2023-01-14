d0f7a7989eaeda4967e7d140bd6e4eb1ef093ee5
The-ARJ <joshiaayush871@gmail.com>
2023-01-14 12:51:01 +0545

    improved security and errorhandling

--- controllers/food-controller.js
+++ controllers/food-controller.js
@@ -3,14 +3,18 @@ const Food = require("../models/Food");
 const getAllFoods = (req, res, next) => {
   Food.find()
     .then((foods) => {
-      res.json(foods);
+      res
+        .status(200)
+        .json({ message: "All foods retrieved successfully", foods });
     })
-    .catch(next);
+    .catch((error) => {
+      res.status(500).json({ message: "Error retrieving foods", error });
+    });
 };
 
 const createFood = (req, res, next) => {
-  console.log(req.body)
-  console.log(req.user)
+  // console.log(req.body);
+  // console.log(req.user);
   let food = {
     name: req.body.name,
     // image: req.body.image,
@@ -18,63 +22,84 @@ const createFood = (req, res, next) => {
     recipe: req.body.recipe,
     calories: req.body.calories,
     owner: req.user.id,
-    
-  }
+  };
   Food.create(food)
     .then((food) => {
-      res.status(201).json(food);
+      res.status(201).json({
+        message: "Food created successfully",
+        food,
+      });
     })
-    .catch('next');
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error creating food",
+        error: error.message,
+      });
+    });
 };
 
 const deleteAllFoods = (req, res, next) => {
   Food.deleteMany()
     .then((status) => {
-      res.json(status);
+      res
+        .status(200)
+        .json({ message: "All Foods deleted successfully", status });
     })
-    .catch(next);
+    .catch((error) => {
+      res.status(500).json({ message: "Error deleting all foods", error });
+    });
 };
 
 const getFoodById = (req, res, next) => {
   Food.findById(req.params.food_id)
-    .populate("category")
+    // .populate("category")
     .then((food) => {
-      res.json(food);
+      if (!food) {
+        return res.status(404).json({ message: "Food not found" });
+      }
+      res.json({ message: "Food retrieved successfully", food });
     })
-    .catch(next);
+    .catch((error) => {
+      res.status(500).json({ message: "Error retrieving food", error });
+    });
 };
 
 const updateFoodById = (req, res, next) => {
   Food.findById(req.params.food_id)
     .then((food) => {
-      if (food.owner != req.user.id) {
-        res.status(403);
-        return next(new Error("Not allowed"));
+      if (!food) {
+        res.status(404).json({ message: "Food not found" });
+      } else if (food.owner != req.user.id) {
+        res.status(403).json({ message: "Not allowed" });
+      } else {
+        food.name = req.body.name ? req.body.name : food.name;
+        food.image = req.body.image ? req.body.image : food.image;
+        food.calories = req.body.calories ? req.body.calories : food.calories;
+        food.recipe = req.body.recipe ? req.body.recipe : food.recipe;
+        food.meal = req.body.meal ? req.body.meal : food.meal;
+        food
+          .save()
+          .then((food) =>
+            res.json({ message: "Food updated successfully", food })
+          )
+          .catch(next);
       }
-      food.foodname = req.body.foodname ? req.body.foodname : food.foodname;
-      food.image = req.body.image ? req.body.image : food.image;
-      food.calories = req.body.calories ? req.body.calories : food.calories;
-      food.recipe = req.body.recipe ? req.body.recipe : food.recipe;
-      food.category = req.body.category ? req.body.category : food.category;
-      food
-        .save()
-        .then((food) => res.json(food))
-        .catch(next);
     })
     .catch(next);
-
-  // Food.findByIdAndUpdate(req.params.food_id, { $set: req.body }, { new: true })
-  //     .then((food) => {
-  //         res.json(food)
-  //     }).catch(next)
 };
 
 const deleteFoodById = (req, res, next) => {
   Food.findByIdAndDelete(req.params.food_id)
     .then((food) => {
-      res.json(food);
+      if (food) {
+        res.json({ message: "Food item deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Food item not found" });
+      }
     })
-    .catch(next);
+    .catch((error) => {
+      res.status(500).json({ message: "Error deleting food item", error });
+    });
 };
 
 module.exports = {
