320c657a514261e16204fb964856ba99f2a513d9
The-ARJ <joshiaayush871@gmail.com>
2023-01-15 20:06:25 +0545

    notification model

---
+++ models/Notification.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
