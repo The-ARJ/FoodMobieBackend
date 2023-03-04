d1c127ccec7102f5b9ed381a3bf2a80b60013edc
The-ARJ <joshiaayush871@gmail.com>
2023-03-04 20:35:08 +0545

    user test

--- test/users.test.js
+++ test/users.test.js
@@ -2,37 +2,37 @@ const supertest = require("supertest");
 const app = require("../app");
 const User = require("../models/User");
 const api = supertest(app);
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const user = {
-  username: "testUser",
-  password: "testUser",
+  email: "testUser1",
+  password: "testUser1",
 };
-beforeAll(async()=>{
-    await User.deleteMany({})
-})
+beforeAll(async () => {
+  await User.deleteMany({});
+});
 
 test("user registration", async () => {
   await api
-    .post("/users/register")
+    .post("/users")
     .send(user)
     .expect(201)
     .expect((res) => {
-    //   console.log(res.body);
-    expect(res.body.status).toContain('User registration success.')
+      //   console.log(res.body);
+      expect(res.body.status).toContain("User registration success.");
     });
 });
 test("user login", async () => {
-    await api
-    .post("/users/login")
+  await api
+    .post("/users/login/user")
     .send(user)
     .expect(200)
-    // .expect((res) => {
-    // // console.log(res.body);
-    // expect(res.body.status).toContain("Login Success");
-    // expect(res.body.data).toHaveProperty("token");
-    // });
+    .expect((res) => {
+      console.log(res.body);
+      expect(res.body.status).toContain("Login Success");
+      // expect(res.body.data).toHaveProperty("token");
     });
-  
-afterAll(async()=>{
-    await mongoose.connection.close()
-})
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
